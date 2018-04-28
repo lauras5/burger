0740cd14d67423508b85fb77d6ecb94ee5c66318
Laura Salcido <idlaurasalcido@gmail.com>
2018-04-28 15:06:44 -0700

    bugs

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // import mysql npm
 var mysql = require('mysql');
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else { connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+
+}
 // set up mysql connection
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-});
 
 // make connection
 connection.connect(function (e) {
